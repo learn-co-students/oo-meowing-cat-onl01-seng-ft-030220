997e4f4500901ba1fbf89d7416fd9ae3a3199a74
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-12 23:42:13 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_reader :name
+  attr_writer :name
+end
+
+class Cat
+  attr_accessor
+  def meow
+    puts "meow!"
+  end
+end
+
+
+
+maru = Cat.new
+
+maru.meow
